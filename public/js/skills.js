04065c61119d8a3e4780328008987f72a6150863
DigglerGhost999 <99803118+DigglerGhost999@users.noreply.github.com>
2022-05-20 13:45:52 +0100

    skills js

---
+++ public/js/skills.js
@@ -0,0 +1,26 @@
+class skills {
+   
+    constructor(id,name, description,range,area,cost,damage ,target) {
+        this.name = name;
+        this.id= id;
+        this.description= description;
+        this.range= range;
+        this.area= area;
+        this.cost= cost;
+        this.damage= damage;
+        this.target= target;
+    }
+
+    target(){
+
+    }
+
+    movement(){
+
+    }
+
+    damage(){
+        
+    }
+
+}
